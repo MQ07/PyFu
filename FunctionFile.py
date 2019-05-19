5671a608b82166e2203d2c32f8bf35b595496348
“MQ07” <csu2050@gmail.com>
2019-05-19 16:08:40 -0400

    added PyFu program

--- FunctionFile.py
+++ FunctionFile.py
@@ -1,11 +1,1 @@
-class Robot:
-    def introduce_self(self):
-        print("My name is " + self.name)
 
-
-r1 = Robot()
-r1.name = "Mohammed Qadeer"
-r1.color = "red"
-r1.weight = 30
-
-r1.introduce_self()
